b8b7d8cf95b71f16de360037c431b7a0d16ca0a8
Andrew Cho <cisdell@gmail.com>
2023-09-25 17:51:06 -0400

    added base model and full name property of the class

--- models.py
+++ models.py
@@ -34,7 +34,7 @@ class NearEarthObject:
     """
     # TODO: How can you, and should you, change the arguments to this constructor?
     # If you make changes, be sure to update the comments in this file.
-    def __init__(self, **info):
+    def __init__(self, **kwargs):
         """Create a new `NearEarthObject`.
 
         :param info: A dictionary of excess keyword arguments supplied to the constructor.
@@ -44,10 +44,10 @@ class NearEarthObject:
         # You should coerce these values to their appropriate data type and
         # handle any edge cases, such as a empty name being represented by `None`
         # and a missing diameter being represented by `float('nan')`.
-        self.designation = ''
-        self.name = None
-        self.diameter = float('nan')
-        self.hazardous = False
+        self.designation = kwargs['pdes']
+        self.name = kwargs['name']
+        self.diameter = float(kwargs['diameter'])
+        self.hazardous = kwargs['pha']
 
         # Create an empty initial collection of linked approaches.
         self.approaches = []
@@ -56,7 +56,11 @@ class NearEarthObject:
     def fullname(self):
         """Return a representation of the full name of this NEO."""
         # TODO: Use self.designation and self.name to build a fullname for this object.
-        return ''
+        full_name = self.designation
+        if self.name:
+            full_name += f' ({self.name})'
+
+        return full_name
 
     def __str__(self):
         """Return `str(self)`."""
